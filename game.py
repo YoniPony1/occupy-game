0a518e7366e8179a769bbef42e8b1d1ace2a0705
YoniPony1 <132946635+YoniPony1@users.noreply.github.com>
2023-05-14 19:06:28 +0300

    Update game.py

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ import pygame
 
 pygame.init()
 
+
 # button class
 class Button():
     def __init__(self, text, color, font, x, y, image, width, height, align = "center"):
@@ -28,7 +29,8 @@ class Button():
         game.window.blit(self.image, self.image_rect)
         game.window.blit(self.text, self.text_rect)
 
-#maingame class
+
+# main game class
 class Game():
     def __init__(self):
         # start window
@@ -38,36 +40,45 @@ class Game():
         self.window_height = 0.85*self.monitor_height
         pygame.display.set_caption("game")
         self.window = pygame.display.set_mode((self.window_width, self.window_height))
+        # sets max width
+        if self.monitor_width/self.monitor_height > 1.78:
+            self.max_width = (56/45)*self.monitor_height
+            self.max_width_start = 0.5*(self.window_width-self.max_width)
+        else:
+            self.max_width = self.window_width
+            self.max_width_start = 0
 
         # timer
         self.clock = pygame.time.Clock()
         # fonts
-        self.big_font = pygame.font.SysFont("Ariel", 80)
-        self.small_font = pygame.font.SysFont("Ariel", 40)
+        self.big_font = pygame.font.SysFont("Ariel", int(0.8*0.055*self.max_width))
+        self.small_font = pygame.font.SysFont("Ariel", int(0.8*0.033*self.max_width))
         # images
-        self.backgroung = pygame.transform.scale(pygame.image.load("Background.png"),(self.window_width, self.window_height))
+        self.background = pygame.transform.scale(pygame.image.load("Background.png"), (self.window_width, self.window_height))
         self.button = pygame.image.load("orange_button.png")
 
-
         # vars
-        self.rect_drawn = False
+        self.board_area = []
+        self.player_drawn = False
+        self.p_velocity = 0.2*round(0.8*self.max_width/25)
         self.in_field = False
+        self.field = []
         # pages vars
         self.main_menu = True
         self.game = False
 
-
     def draw_board(self):
         # board
-        board_width = round(0.8*self.window_width/25)*25  # make sure it's divided by 20
-        board_height = 0.68*board_width
-        board_x, board_y = (self.window_width-board_width)/2, 0.35*(self.window_height-board_height)
+        board_width = round(0.8*self.max_width/25)*25  # make sure it's divided by 25
+        board_height = round(0.68*board_width)
+        board_x, board_y = (self.max_width-board_width)/2+self.max_width_start, (self.window_height-1.12*board_height)/2 - 0.5*0.0051*board_height
         board = pygame.Rect(board_x, board_y, board_width, board_height)
         pygame.draw.rect(self.window, "#4a1486", board)
         board_x_end, board_y_end = board_x+board_width-1, board_y+board_height-1
+        self.board_area = [board_x, board_y, board_x+board_width-1, board_y+board_height-1]
 
         # outline
-        outline_thick = 5
+        outline_thick = round(0.0051*board_height)
         outline_width = board_width+outline_thick*2+1
         outline_height = board_height+outline_thick*2+1
         outline_pos = (board_x-1-outline_thick, board_y-1-outline_thick)
@@ -75,7 +86,7 @@ class Game():
         pygame.draw.rect(self.window, "black", outline, outline_thick, 5)
 
         # guide lines
-        #-------------
+        # -------------
         color = "blue"
         gap = board_width/25
         # vertical lines
@@ -91,52 +102,49 @@ class Game():
         board_background = pygame.transform.scale(pygame.image.load("pattern2_21x17.png"), (board_width - 2 * gap-1, board_height - 2 * gap-1))
         self.window.blit(board_background, (board_x + gap, board_y + gap))
 
-        #player
-        if not self.rect_drawn:  # checks if it the first time its drawn
-            #self.rect = pygame.Rect(board_x+12*gap, board_y_end - gap + 1, gap - 1, gap - 1)
+        # player
+        if not self.player_drawn:  # checks if it the first time its drawn
             self.player = pygame.transform.scale(pygame.image.load("ball3.png"), (gap-1, gap-1))
-            self.rect = self.player.get_rect()
-            self.rect.topleft = (board_x+12*gap, board_y_end - gap + 1)
-            self.rect_drawn = True
-        self.window.blit(self.player, self.rect)
+            self.player_rect = self.player.get_rect()
+            self.player_rect.topleft = (board_x+12*gap, board_y_end - gap + 1)
+            self.player_drawn = True
+        self.window.blit(self.player, self.player_rect)
 
-        #button
-        button_1 = Button("Back", "white", self.small_font, self.window_width/2 , 0.936*self.window_height, self.button, 200, 60)
+        # button
+        button_1 = Button("Back", "white", self.small_font, 0.5*self.max_width+self.max_width_start , board_y+1.09*outline_height, self.button, 0.2*outline_height, 0.06*outline_height)
         button_1.draw()
         if button_1.clicked():
             self.main_menu = True
             self.game = False
 
-
     def draw_main_menu(self):
-        play_btn = Button("Play", "white",self.big_font, self.window_width/2, 0.35*self.window_height, self.button, 380, 100)
+        play_btn = Button("Play", "white", self.big_font, 0.5*self.max_width+self.max_width_start, 0.35*self.window_height, self.button, 0.212*self.max_width, 0.055*self.max_width)
         play_btn.draw()
         if play_btn.clicked():
             self.main_menu = False
             self.game = True
 
-
     def main_loop(self):
         run = True
         while run:
-            velocity = 10
 
-            #clock
+            # clock
             self.clock.tick(60)
 
-            #draw
-            self.window.blit(self.backgroung, (0, 0))
+            # draw
+            self.window.blit(self.background, (0, 0))
             if self.main_menu:
                 self.draw_main_menu()
             else:
                 self.draw_board()
             pygame.display.update()
 
-            #key inputs
+            # key inputs
             keys = pygame.key.get_pressed()
             if self.game:  # only when in game
                 # arrow keys input
                 if any((keys[pygame.K_LEFT], keys[pygame.K_RIGHT], keys[pygame.K_UP], keys[pygame.K_DOWN])):
+                    direction = (0, 0)
                     if keys[pygame.K_LEFT]:
                         direction = (-1, 0)
                     if keys[pygame.K_RIGHT]:
@@ -147,20 +155,35 @@ class Game():
                         direction = (0, 1)
 
                     if self.in_field:
-                        #mvement in the field
+                        # movement in the field
                         print(1)
                     else:
-                        #movement in occupied area
-                        print(0)
-                        player_des = 0
+                        # movement in occupied area
+                        player_des_x = self.player_rect.x+self.p_velocity*direction[0]
+                        player_des_y = self.player_rect.y+self.p_velocity*direction[1]
+                        print(f"x:{player_des_x}, y:{player_des_y}")
+                        # limit to the board area
+                        if player_des_x > (self.board_area[2]-self.player_rect[2]):
+                            player_des_x = (self.board_area[2]-self.player_rect[2])
+                        elif player_des_x < self.board_area[0]:
+                            player_des_x = self.board_area[0]
+                        if player_des_y > (self.board_area[3]-self.player_rect[3]):
+                            player_des_y = (self.board_area[3]-self.player_rect[3])
+                        elif player_des_y < self.board_area[1]:
+                            player_des_y = self.board_area[1]
+                        # moves player
+                        self.player_rect.x = player_des_x
+                        self.player_rect.y = player_des_y
 
-
-
-            #event handler
+            # event handler
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        run = False
 
 
 game = Game()
 game.main_loop()
+pygame.quit()
