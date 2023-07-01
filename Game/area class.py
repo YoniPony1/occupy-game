31795289095a6030921a694fb07ef83e1bd72024
YoniPony1 <132946635+YoniPony1@users.noreply.github.com>
2023-07-01 20:44:20 +0300

    Field class

--- Game/area class.py
+++ Game/area class.py
@@ -1,25 +1,37 @@
 import pygame
-
 pygame.init()
 
 
-class Area:
-    def __init__(self, board_frame):
-        # board variables
-        self.bx = board_frame.x
-        self.by = board_frame.y
-        self.bw = board_frame.width
-        self.bh = board_frame.height
-        # area rectangle
-        y = self.by + 0.05 * self.bh
-        x = self.bx + y
-        height = 0.9 * self.bh
-        width = self.bw - 2 * (x - self.bx)
-        self.area_rect = pygame.Rect(x, y, width, height)
-        # 4 default points
-        self.default = [(x, y), (x+width, y), (x+width, y+height), (x, y+height)]
+class Field:
+    def __init__(self, points):
+        # POINTS
+        # list of all filed points in x,y tuples
+        # [(x1, y1), (x2, y2), (x3, y3)....]
+        self.points = points
+        # LINES
+        # list of all field lines. each line is a list of 2 points
+        # [[(x1,y1), (x2,y2)], [(x1,y1), (x2,y2)], [(x1,y1), (x2,y2)]...]
+        self.lines = self.lines()
+        # vertical lines
+        self.ver_lines = self.vertical_lines()
 
-        self.points = []
+    def lines(self):
+        lines = []
+        for i in range(len(self.points) - 1):
+            lines.append([self.points[i], self.points[i+1]])
+        lines.append([self.points[-1], self.points[0]])
+        return lines
 
+    def vertical_lines(self):
+        ver_lines = []
+        if self.lines[0][0][1] == self.lines[0][1][1]:
+            # first line is vertical
+            first_line = 0
+        else:
+            # first line is horizontal
+            first_line = 1
+        for i in range(len(self.lines)//2):
+            ver_lines.append(self.lines[first_line+2*i])
 
-area = Area(pygame.Rect(0, 0, 0, 0))
+        return ver_lines
+
